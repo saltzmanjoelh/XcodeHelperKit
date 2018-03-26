4ae8a6a1ae31d6d324ca17cc1b7c42f2127b9d10
Joel Saltzman <talk@joelsaltzman.com>
2018-03-26 00:18:44 -0700

    Added static lib type

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "XcodeHelperKit",
+            type: .static,
             targets: ["XcodeHelperKit"]),
     ],
     dependencies: [
