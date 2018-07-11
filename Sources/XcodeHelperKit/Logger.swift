57ac86384462699b1f42e0365144285c527d92ce
Joel Saltzman <talk@joelsaltzman.com>
2018-07-11 01:07:58 -0700

    Fixed variable args being logged as an array

--- Sources/XcodeHelperKit/Logger.swift
+++ Sources/XcodeHelperKit/Logger.swift
@@ -24,9 +24,10 @@ public struct Logger {
     
     private let logSystem: OSLog?
     public init(category: String){
-        let pid = ProcessInfo.processInfo.processIdentifier
+//        let pid = ProcessInfo.processInfo.processIdentifier
+//        print("pid: \(pid)")
         if #available(OSX 10.12, *) {
-            logSystem = OSLog.init(subsystem: "com.joelsaltzman.XcodeHelper.plist", category: category)
+            logSystem = OSLog.init(subsystem: Logger.subsystemIdentifier, category: category)
         } else {
             // Fallback on earlier versions
             logSystem = nil
@@ -36,57 +37,86 @@ public struct Logger {
     public func logWithNotification(_ message: StaticString, _ args: CVarArg...) -> UUID? {
         var uuid: UUID?
         if #available(OSX 10.12, *) {
-            uuid = log(type: .default, message: message, args: args)
+            uuid = log(type: .default, message: message, args)
         } else {
             // Fallback on earlier versions
-            uuid = log(type: OSLogType.init(0), message: message, args: args)
+            uuid = log(type: OSLogType.init(0), message: message, args)
         }
         if let theUUID = uuid {
-            displayNotification(withMessage: message, args: args, uuid: theUUID)
+            displayNotification(uuid: theUUID, withMessage: message, args)
         }
         return uuid
     }
     @discardableResult
     public func error(_ message: StaticString, _ args: CVarArg...) -> UUID? {
         if #available(OSX 10.12, *) {
-            return log(type: .error, message: message, args: args)
+            return log(type: .error, message: message, args)
         } else {
             // Fallback on earlier versions
-            return log(type: OSLogType.init(16), message: message, args: args)
+            return log(type: OSLogType.init(16), message: message, args)
         }
     }
     @discardableResult
     public func log(_ message: StaticString, _ args: CVarArg...) -> UUID? {
         if #available(OSX 10.12, *) {
-            return log(type: .default, message: message, args: args)
+            return log(type: .default, message: message, args)
         } else {
             // Fallback on earlier versions
-            return log(type: OSLogType.init(0), message: message, args: args)
+            return log(type: OSLogType.init(0), message: message, args)
         }
     }
-    func log(type: OSLogType, message: StaticString, args: CVarArg...) -> UUID? {
+//    public func log(type: OSLogType, message: StaticString, args: CVarArg...) -> UUID? {
+//        let uuid = UUID()
+//        if #available(OSX 10.12, *) {
+//            if let system = logSystem {
+//                os_log(message, log: system, type: type, args)
+////                os_log(message, log: system, type: type, args)
+//            }else{
+//                print(message)
+//            }
+//        }else{
+//            print(message)
+//        }
+//
+//        return uuid
+//    }
+    private func log(type: OSLogType, message: StaticString, _ args: [CVarArg]) -> UUID? {
         let uuid = UUID()
         if #available(OSX 10.12, *) {
             if let system = logSystem {
-                os_log(message, log: system, type: type, args)
-//                os_log(message, dso: #dsohandle, log: system, type: type, args: args)
-            }else{
-                print(message)
+                switch args.count {
+                case 0:
+                    os_log(message, log: system, type: type)
+                case 1:
+                    os_log(message, log: system, type: type, args[0])
+                case 2:
+                    os_log(message, log: system, type: type, args[0], args[1])
+                case 3:
+                    os_log(message, log: system, type: type, args[0], args[1], args[2])
+                case 4:
+                    os_log(message, log: system, type: type, args[0], args[1], args[2], args[3])
+                case 5:
+                    os_log(message, log: system, type: type, args[0], args[1], args[2], args[3], args[4])
+                default:
+                    os_log(message, log: system, type: type, args)
+                }
+            } else {
+                print(compileFormatString(message, args))
             }
-        }else{
-            print(message)
+        } else {
+            print(compileFormatString(message, args))
         }
-        
         return uuid
     }
-    func displayNotification(withMessage message: StaticString, args: CVarArg..., uuid: UUID) {
+    func displayNotification(uuid: UUID, withMessage message: StaticString, _ args: [CVarArg]) {
         //UserDefaults.standard.addSuite(named: "com.joelsaltzman.XcodeHelper")
         let defaults = Bundle.main.bundleIdentifier == "com.joelsaltzman.XcodeHelper" ? UserDefaults.standard : UserDefaults.init(suiteName: "com.joelsaltzman.XcodeHelper")
         if defaults?.bool(forKey: Logger.UserDefaultsKey) == false {
             return
         }
         let notification = NSUserNotification()
-        notification.informativeText = message.description
+        
+        notification.informativeText = compileFormatString(message, args)
         //        notification.soundName = NSUserNotificationDefaultSoundName
         notification.actionButtonTitle = "Silence"
         //        if let directory = logsDirectory {
@@ -112,6 +142,24 @@ public struct Logger {
                 })
                 Logger.timers[uuid] = timer
             }
+        }
+    }
+    func compileFormatString(_ message: StaticString, _ args: [CVarArg]) -> String {
+        switch args.count {
+        case 0:
+            return String.init(format: message.description, args[0])
+        case 1:
+            return String.init(format: message.description, args[0])
+        case 2:
+            return String.init(format: message.description, args[0], args[1])
+        case 3:
+            return String.init(format: message.description, args[0], args[1], args[2])
+        case 4:
+            return String.init(format: message.description, args[0], args[1], args[2], args[3])
+        case 5:
+            return String.init(format: message.description, args[0], args[1], args[2], args[3], args[4])
+        default:
+            return String.init(format: message.description, args)
         }
     }
     public func logStringFromProcessResults(_ processResults: [ProcessResult]) -> String {
